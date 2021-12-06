1d2bd31125be513a7167b604975a040b6f68226f
barnstee <erichb@microsoft.com>
2021-12-06 15:22:27 +0100

    1. Debounce graph rendering. 2. Update NuGet packages. 3. Added enqueue time when OPC UA timestamp is unavailable.

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -14,27 +14,53 @@ namespace OpcUaWebDashboard.Controllers
     public class DashboardController : Controller
     {
         private static IHubContext<StatusHub> _hubContext;
-        private static List<Tuple<string, string, string>> _latestTelemetry;
-        private static Stopwatch _tableTimer;
+
+        private static List<Tuple<string, string, string>> _latestTelemetry = new List<Tuple<string, string, string>>();
+
+        private static Stopwatch _tableTimer = new Stopwatch();
+        private static Stopwatch _graphTimer = new Stopwatch();
+
         private static bool _pageReloaded = false;
 
         public DashboardController(IHubContext<StatusHub> hubContext)
         {
             _hubContext = hubContext;
-            _latestTelemetry = new List<Tuple<string, string, string>>();
-            _tableTimer = new Stopwatch();
             _tableTimer.Start();
+            _graphTimer.Start();
         }
 
         public static void AddDatasetToChart(string name)
         {
-            _hubContext.Clients.All.SendAsync("addDatasetToChart", name).GetAwaiter().GetResult();
-            Debug.WriteLine("Sent new data set: " + name);
+            if (_hubContext != null)
+            {
+                _hubContext.Clients.All.SendAsync("addDatasetToChart", name).GetAwaiter().GetResult();
+                Debug.WriteLine("Sent new data set: " + name);
+            }
         }
 
         public static void AddDataToChart(string timestamp, string[] values)
         {
-            _hubContext.Clients.All.SendAsync("addDataToChart", timestamp, values).GetAwaiter().GetResult();
+            if (_hubContext != null)
+            {
+                // update graph every second
+                if (_graphTimer.ElapsedMilliseconds > 1000)
+                {
+                    // if the page was reloaded in the mean-time, resend the data sets
+                    if (_pageReloaded == true)
+                    {
+                        foreach (string nodeId in MessageProcessor.NodeIDs)
+                        {
+                            AddDatasetToChart(nodeId);
+                        }
+
+                        _pageReloaded = false;
+                    }
+
+                    _hubContext.Clients.All.SendAsync("addDataToChart", timestamp, values).GetAwaiter().GetResult();
+
+                    _graphTimer.Restart();
+                }
+            }
         }
 
         public ActionResult Privacy()
@@ -51,65 +77,57 @@ namespace OpcUaWebDashboard.Controllers
 
         public static void CreateTableForTelemetry(List<Tuple<string,string, string>> telemetry)
         {
-            foreach (Tuple<string, string, string> item in telemetry)
+            if (_hubContext != null)
             {
-                bool found = false;
-                for (int i = 0; i < _latestTelemetry.Count; i++)
+                foreach (Tuple<string, string, string> item in telemetry)
                 {
-                    if (_latestTelemetry[i].Item1 == item.Item1)
+                    bool found = false;
+                    for (int i = 0; i < _latestTelemetry.Count; i++)
                     {
-                        _latestTelemetry[i] = item;
-                        found = true;
-                        break;
+                        if (_latestTelemetry[i].Item1 == item.Item1)
+                        {
+                            _latestTelemetry[i] = item;
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        _latestTelemetry.Add(item);
                     }
                 }
 
-                if (!found)
+                // update table every second
+                if (_tableTimer.ElapsedMilliseconds > 1000)
                 {
-                    _latestTelemetry.Add(item);
-                }
-            }
+                    // create HTML table
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("<table width='800px' cellpadding='3' cellspacing='3'>");
 
-            // update table every second
-            if (_tableTimer.ElapsedMilliseconds > 1000)
-            {
-                // create HTML table
-                StringBuilder sb = new StringBuilder();
-                sb.Append("<table width='800px' cellpadding='3' cellspacing='3'>");
-
-                // header
-                sb.Append("<tr>");
-                sb.Append("<th><b>OPC UA Node ID</b></th>");
-                sb.Append("<th><b>Latest Value</b></th>");
-                sb.Append("<th><b>Time Stamp</b></th>");
-                sb.Append("</tr>");
-
-                // rows
-                foreach (Tuple<string, string, string> item in _latestTelemetry)
-                {
+                    // header
                     sb.Append("<tr>");
-                    sb.Append("<td style='width:400px'>" + item.Item1 + "</td>");
-                    sb.Append("<td style='width:200px'>" + item.Item2 + "</td>");
-                    sb.Append("<td style='width:200px'>" + item.Item3 + "</td>");
+                    sb.Append("<th><b>OPC UA Node ID</b></th>");
+                    sb.Append("<th><b>Latest Value</b></th>");
+                    sb.Append("<th><b>Time Stamp</b></th>");
                     sb.Append("</tr>");
-                }
 
-                sb.Append("</table>");
-
-                _hubContext.Clients.All.SendAsync("addTable", sb.ToString()).GetAwaiter().GetResult();
-
-                // if the page was reloaded in the mean-time, resend the data sets
-                if (_pageReloaded == true)
-                {
-                    foreach (string nodeId in MessageProcessor.NodeIDs)
+                    // rows
+                    foreach (Tuple<string, string, string> item in _latestTelemetry)
                     {
-                        AddDatasetToChart(nodeId);
+                        sb.Append("<tr>");
+                        sb.Append("<td style='width:400px'>" + item.Item1 + "</td>");
+                        sb.Append("<td style='width:200px'>" + item.Item2 + "</td>");
+                        sb.Append("<td style='width:200px'>" + item.Item3 + "</td>");
+                        sb.Append("</tr>");
                     }
 
-                    _pageReloaded = false;
-                }
+                    sb.Append("</table>");
 
-                _tableTimer.Restart();
+                    _hubContext.Clients.All.SendAsync("addTable", sb.ToString()).GetAwaiter().GetResult();
+
+                    _tableTimer.Restart();
+                }
             }
         }
     }
